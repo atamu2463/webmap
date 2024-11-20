9bba09e5187c21f8520861b0a4f6f06978b969a7
asa-cording <asahiwork.cording@gmail.com>
2024-11-21 08:11:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# webmap of MapLibre GL JS
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:5173](http://localhost:5173) with your browser to see the result.
+
+You can start editing the page by modifying `src/main.ts`. The page auto-updates as you edit the file.
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { Map } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+// MapLibre GL JSのMapインスタンスを初期化＝地図画面を作成
+const map = new Map({
+    container: 'map',
+    style: {
+        version: 8,
+        sources: {
+            osm: { // OpenStreetMapのタイルデータを定義
+                type: 'raster',
+                tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+                tileSize: 256,
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+            },
+        },
+        layers: [
+            {// OpenStreetMapのタイルデータを表示
+                id: 'osm',
+                type: 'raster',
+                source: 'osm',
+            },
+        ],
+    },
+    center: [143.95, 43.65],
+    zoom: 6,
+});
