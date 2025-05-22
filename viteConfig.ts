e21b1054d8dcdcdeffb8fb4b21a9c658b4e72469
asa-cording <asahiwork.cording@gmail.com>
2025-05-22 13:13:50 +0900

    fixed

--- viteConfig.ts
+++ viteConfig.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 
+const REPO_NAME = 'webmap'; 
 
 export default defineConfig({
   plugins: [], 
-  base: '/webmap/', 
+  base: `/${REPO_NAME}/`, 
   build: {
     outDir: 'dist', 
   },
